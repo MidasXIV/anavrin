b8449b9e8cda6fd4c766207235e27a67281d14be
Kartik Nair <kartiknair1911@gmail.com>
2020-08-04 21:33:42 +0400

    Migrate completely to Tailwind

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,14 +3,14 @@ import AppHeader from "../components/app-header";
 
 const Dashboard: FC = () => {
   return (
-    <main>
+    <>
       <AppHeader
         title="Hello there"
         description="You can see your portfolios estimated value & progress below:"
         currentPage={{ label: "Home", path: "/dashboard" }}
-        otherPages={[{ label: "Portfolio", path: "/dashboard/portfolio" }]}
+        otherPages={[{ label: "Portfolio", path: "/portfolio" }]}
       />
-    </main>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/components/**/*.{js,ts,jsx,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {},
   plugins: []
