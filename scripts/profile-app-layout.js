b827abad452b5680ce85886908f3ee3ea68da8b5
MidasXIV <amanshaikh.arc@gmail.com>
2020-08-06 00:55:53 +0400

    Update profile-app-layout script to use decouple logic using controllers * Add Stdio dependency to take user inputs

--- scripts/profile-app-layout.js
+++ scripts/profile-app-layout.js
@@ -4,9 +4,103 @@
  * 2. goto each endpoint mentioned in sitemap
  * 3. cature each page in different view ports
  */
+
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const puppeteer = require("puppeteer-core");
+const stdio = require("stdio");
+const fs = require("fs");
+const APP_PAGES_LIST = require("../public/sitemap.json");
+
+const LAYOUT_PROFILE_DIRECTORY = "profile-layout";
+
+/**
+ * usage example
+ * node <your_script.js> -u https://google.com
+ * Then ops object will be as follows: { url: 'https://google.com' }
+ */
+
+const options = stdio.getopt({
+  url: { key: "u", args: 1, description: "base URL of app to profile" }
+});
+
+/**
+ * Loading Application Config
+ */
+const appBaseURL = options.url;
+
+/** *************************************************************
+ *                     Helper functions
+ ************************************************************** */
+
+const formatMessage = (message, type) => {
+  switch (type) {
+    case "s":
+      console.log(`\x1b[33m%s\x1b[0m`, message, `\x1b[0m`);
+      break;
+    case "m":
+      console.log(`\x1b[35m`, message, `\x1b[0m`);
+      break;
+    default:
+      console.log(message);
+  }
+  // console.log(`\x1b[0m`); // reset console color
+};
+
+/** *************************************************************
+ *                     Controller functions
+ ************************************************************** */
+
+async function closeHeadlesssChrome(browserObj) {
+  formatMessage("-- Trying to Close Chome Headless Window");
+  await browserObj.close();
+}
+
+async function setChromeViewport(pageObj) {
+  formatMessage("-- Trying to Update page viewPort");
+  await pageObj.setViewport({
+    width: 1366,
+    height: 738,
+    deviceScaleFactor: 1,
+    isMobile: false,
+    hasTouch: false,
+    isLandscape: false
+  });
+}
+
+async function setUserAgent(pageObj) {
+  formatMessage("-- Setting User Agent");
+  pageObj.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+}
+
+const takeScreenshot = async (pageObj, srcUrl) => {
+  formatMessage(`-- Trying to Load Web Page ${srcUrl}`);
+  await pageObj.goto(srcUrl);
+
+  formatMessage("-- Trying to Take Screenshot");
+  await pageObj.screenshot({
+    path: `${LAYOUT_PROFILE_DIRECTORY}/default.png`,
+    clip: {
+      x: 0,
+      y: 0,
+      width: 795,
+      height: 1125
+    }
+  });
+};
+
+/** *************************************************************
+ *                     Main Workflow
+ ************************************************************** */
+
+if (!fs.existsSync(LAYOUT_PROFILE_DIRECTORY)) {
+  formatMessage(`creating layout-profile directory`, "s");
+  fs.mkdirSync(LAYOUT_PROFILE_DIRECTORY);
+}
+
+formatMessage(`Base URL of app :: ${appBaseURL}`);
 
 const createServer = async html => {
   const launchOptions = {
@@ -19,19 +113,17 @@ const createServer = async html => {
   const browser = await puppeteer.launch(launchOptions);
   const page = await browser.newPage();
 
+  formatMessage(await browser.version(), "m");
+
   // set viewport and user agent (just in case for nice viewing)
-  await page.setViewport({ width: 1366, height: 768 });
-  await page.setUserAgent(
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-  );
+  await setChromeViewport(page);
+  await setUserAgent(page);
 
   // go to the target web
   await page.goto("https://google.com");
-
-  console.log(await browser.version());
-
+  await takeScreenshot(page, "https://google.com");
   // close the browser
-  await browser.close();
+  await closeHeadlesssChrome(browser);
 };
 
 createServer();
