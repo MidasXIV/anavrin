2e923b6e6b5bc71beb3f92221f6e8675cf3734fe
MidasXIV <amanshaikh.arc@gmail.com>
2020-10-31 00:31:20 +0400

    Update header styling and layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -16,8 +16,17 @@ const Header: FC<HeaderProps> = ({ title, description }) => {
         </div>
       </div>
       <div className={styles.rightHeader}>
-        <div className={`${styles.content} ${styles.rightBtnPanel}`}>
-          <div className="flex flex-row justify-around w-full h-full">
+        {/* <div className={`${styles.content} ${styles.rightBtnPanel}`}> */}
+        <div className={`${styles.content} ${styles.rightContentPanel}`}>
+          <button className="relative inline-block" type="button">
+            <img
+              className="inline-block w-10 h-10 object-cover rounded-lg"
+              src="//images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+              alt="Profile"
+            />
+            <span className="absolute bottom-0 right-0 inline-block w-3 h-3 -mr-1 bg-green-600 border-2 border-white rounded-full" />
+          </button>
+          {/* <div className="flex flex-row justify-around w-full h-full">
             <button type="button" className={styles.headerBtn}>
               Account
             </button>
@@ -39,7 +48,7 @@ const Header: FC<HeaderProps> = ({ title, description }) => {
             <button type="button" className={styles.headerBtn}>
               Sign out
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
