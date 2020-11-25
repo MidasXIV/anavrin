6472b6a0afc6d655e59c8efef35af4e7db4f95b8
MidasXIV <amanshaikh.arc@gmail.com>
2020-11-26 01:39:29 +0400

    First iteration of bottom panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import Link from "next/link";
 
 const Home: FC = () => (
-  <div className="w-full h-screen bg-gray-200">
+  <div className="w-full h-screen bg-gray-200 flex flex-col">
     {/* <header className="flex items-center px-10 py-12 lg:px-56">
       <a href="#about">
         <h4 className="text-xl">Anavrin</h4>
@@ -39,7 +39,7 @@ const Home: FC = () => (
         </button>
       </div>
     </div> */}
-    <div className="bg-gray-100 flex justify-center">
+    <div className="bg-gray-100 flex justify-center border border-b-1">
       <div className="flex flex-col text-center p-8 md:p-16 max-w-xl">
         <h6 className="uppercase text-xs text-gray-600">coming soon</h6>
         <p className="pt-5">
@@ -63,6 +63,71 @@ const Home: FC = () => (
             >
               Subscribe
             </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="flex-1">
+      <div className="flex flex-row h-full">
+        <div className="p-8 md:p-12 flex w-4/12 flex-col">
+          <h3 className="font-semibold">Check out Anavrin Beta</h3>
+          <button
+            type="button"
+            className="block bg-gray-900 rounded-lg p-2 font-semibold text-gray-100"
+          >
+            <Link href="/dashboard">
+              <a>Open Anavrin!</a>
+            </Link>
+          </button>
+        </div>
+        <div className="bg-gray-100 w-1/12 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="bg-gray-100 w-1/12 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="w-6/12 bg-gray-100 grid grid-cols-3">
+          <div className="border flex justify-center p-4">
+            <p className="text-sm">
+              How much income is my portfolio going to generate this month?
+              <br />
+              What abaout the whole year?
+            </p>
+          </div>
+
+          <div className="border flex justify-center p-4">
+            <p className="text-sm">
+              Which stocks am I depending on the most for my annual dividend income?
+            </p>
+          </div>
+
+          <div className="border flex justify-center p-4">
+            <p className="text-sm">
+              Am I taking on too much risk with my portfolio?
+              <br /> Am i diversified enough?
+            </p>
           </div>
         </div>
       </div>
