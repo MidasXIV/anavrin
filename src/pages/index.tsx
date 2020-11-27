7f31e5a94f77e872a6ef2acbe97dbda14291631d
MidasXIV <amanshaikh.arc@gmail.com>
2020-11-27 23:39:06 +0400

    minor cleanup and fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,8 +68,8 @@ const Home: FC = () => (
       </div>
     </div>
     <div className="flex-1">
-      <div className="flex flex-row h-full">
-        <div className="p-8 md:p-12 flex w-4/12 flex-col">
+      <div className="flex flex-col md:flex-row h-full">
+        <div className="p-8 md:p-12 flex md:w-4/12 flex-col">
           <h3 className="font-semibold">Check out Anavrin Beta</h3>
           <button
             type="button"
@@ -80,50 +80,52 @@ const Home: FC = () => (
             </Link>
           </button>
         </div>
-        <div className="bg-gray-100 w-1/12 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-8"
-          >
-            <path
-              fillRule="evenodd"
-              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+        <div className="flex flex-row h-8 md:w-2/12 md:h-auto">
+          <div className="bg-gray-100 w-1/2 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="bg-gray-100 w-1/2 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-8"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
         </div>
-        <div className="bg-gray-100 w-1/12 flex justify-center hover:text-white hover:bg-gray-900 cursor-pointer transition-colors ease-in duration-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-8"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        <div className="w-6/12 bg-gray-100 grid grid-cols-3">
-          <div className="border flex justify-center p-4">
+        <div className="md:w-6/12 bg-gray-100 grid grid-cols-3">
+          <div className="border flex justify-center p-4 pt-8 md:pt-12">
             <p className="text-sm">
               How much income is my portfolio going to generate this month?
               <br />
-              What abaout the whole year?
+              What about the whole year?
             </p>
           </div>
 
-          <div className="border flex justify-center p-4">
+          <div className="border flex justify-center p-4 pt-8 md:pt-12">
             <p className="text-sm">
               Which stocks am I depending on the most for my annual dividend income?
             </p>
           </div>
 
-          <div className="border flex justify-center p-4">
+          <div className="border flex justify-center p-4 pt-8 md:pt-12">
             <p className="text-sm">
               Am I taking on too much risk with my portfolio?
               <br /> Am i diversified enough?
