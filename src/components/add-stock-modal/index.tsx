dc35952da875cf39063f5e46e8b5aa9a60503bfe
MidasXIV <amanshaikh.arc@gmail.com>
2020-12-02 23:49:56 +0400

    Rename form field and add "Market price" input field

--- src/components/add-stock-modal/index.tsx
+++ src/components/add-stock-modal/index.tsx
@@ -50,8 +50,8 @@ const AddStockModal: FC<AddStockModalProps> = ({ isShowing, cancel }) => {
           </div>
         </div> */}
         <div className="px-2 py-5 sm:px-6 flex flex-col">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-600">Personal details and application.</p>
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Stock Information</h3>
+          {/* <p className="mt-1 max-w-2xl text-sm text-gray-600">Personal details and application.</p> */}
         </div>
         <div className="border-t border-gray-200">
           <dl>
@@ -71,7 +71,19 @@ const AddStockModal: FC<AddStockModalProps> = ({ isShowing, cancel }) => {
               </dd>
             </div>
             <div className="bg-white p-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 text-gray-600">
-              <dt className="text-sm font-medium text-gray-600">Quantity</dt>
+              <dt className="text-sm font-medium text-gray-600">Shares</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2 border border-gray-600 rounded-md">
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  autoComplete="given-name"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </dd>
+            </div>
+            <div className="bg-white p-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 text-gray-600">
+              <dt className="text-sm font-medium text-gray-600">Buy Price</dt>
               <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2 border border-gray-600 rounded-md">
                 <input
                   type="text"
@@ -83,6 +95,16 @@ const AddStockModal: FC<AddStockModalProps> = ({ isShowing, cancel }) => {
               </dd>
             </div>
             <div className="bg-gray-50 py-2 px-6 text-gray-600">
+              <div className="flex w-full text-xs">
+                <div className="flex w-5/12">
+                  <div className="flex-1 pr-3 text-left font-semibold">Dividend</div>
+                  <div className="flex-1 px-3 text-right">0</div>
+                </div>
+                <div className="flex w-7/12">
+                  <div className="flex-1 px-3 text-left font-semibold">Cost Basis</div>
+                  <div className="flex-1 pl-3 text-right">0</div>
+                </div>
+              </div>
               <div className="flex w-full text-xs">
                 <div className="flex w-5/12">
                   <div className="flex-1 pr-3 text-left font-semibold">Open</div>
@@ -121,7 +143,7 @@ const AddStockModal: FC<AddStockModalProps> = ({ isShowing, cancel }) => {
             type="button"
             className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-200 text-red-700 rounded-lg font-semibold text-sm md:ml-2 md:order-2"
           >
-            Delete Account
+            Add Stock
           </button>
           <button
             type="button"
