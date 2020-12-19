c26bbc738dff5d087b6ddef1e4d0f08e85d29edc
MidasXIV <amanshaikh.arc@gmail.com>
2020-12-20 00:43:35 +0400

    Update getStockInfoFunction to cancel previous requests

--- src/util/getStockInformation.ts
+++ src/util/getStockInformation.ts
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+let cancelToken;
 const getStockInformation = async (ticker: string): Promise<any> => {
-  return axios(`/api/dividend/?ticker=${ticker}`);
+  // Check if there are any previous pending requests
+  if (typeof cancelToken !== typeof undefined) {
+    cancelToken.cancel("Operation canceled due to new request.");
+  }
+
+  // Save the cancel token for the current request
+  cancelToken = axios.CancelToken.source();
+
+  return axios(`/api/dividend/?ticker=${ticker}`, { cancelToken: cancelToken.token });
 };
 
 export default getStockInformation;
