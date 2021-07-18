f8e8b3c78f298ab5affee37ca8001519fc74ede5
MidasXIV <amanshaikh.arc@gmail.com>
2021-07-19 00:07:20 +0400

    feat: move api route under binance route

--- src/pages/api/binance.js
+++
@@ -1,5 +0,0 @@
-import BinanceController from "../../controllers/binanceController";
-
-const binanceController = new BinanceController();
-
-export default binanceController.historyUSDT.bind(binanceController);
