552a6783f766034ccb882b2191b993e14dfafa0c
MidasXIV <amanshaikh.arc@gmail.com>
2021-07-24 00:53:03 +0400

    feat: update auth options to store users in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,23 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
   ],
-  debug: false
+  debug: false,
+  database: process.env.MONGODB_URI,
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user, account, profile)
+      return true
+    }
+  },
+  events: {
+    async signIn(message) { /* on successful sign in */ console.log(message); },
+    async signOut(message) { /* on signout */ console.log(message); },
+    async createUser(message) { /* user created */ console.log(message); },
+    async updateUser(message) { /* user updated - e.g. their email was verified */ console.log(message); },
+    async linkAccount(message) { /* account (e.g. Twitter) linked to a user */ console.log(message); },
+    async session(message) { /* session is active */ console.log(message); },
+    async error(message) { /* error in authentication flow */ console.log(message); }
+  }
 }
 
 export default (req, res) => NextAuth(req, res, options)
