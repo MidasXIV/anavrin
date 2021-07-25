37a31863b103bf67bc7a1085a672215be7fa0084
MidasXIV <amanshaikh.arc@gmail.com>
2021-07-26 00:00:10 +0400

    feat: Add Google Auth (#46)
    Allow users to login to Anvarin using their `Google` accounts, using `next-auth`'s Google provider.
    * add mongodb@3.5.9 dependency; this allow next-auth to add accounts, sessions and users to the database provided.
    * new environment variables are added with this PR
       `GOOGLE_CLIENT_ID`, `GOOGLE_CLIENT_SECRET`,`MONGODB_URI`,`NEXTAUTH_URL`.
    
    Related issue: https://github.com/MidasXIV/anavrin/issues/45

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+  ],
+  debug: false,
+  database: process.env.MONGODB_URI,
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user, account, profile)
+      return true
+    }
+  },
+  events: {
+    async signIn(message) { /* on successful sign in */ console.log(message); },
+    async signOut(message) { /* on signout */ console.log(message); },
+    async createUser(message) { /* user created */ console.log(message); },
+    async updateUser(message) { /* user updated - e.g. their email was verified */ console.log(message); },
+    async linkAccount(message) { /* account (e.g. Twitter) linked to a user */ console.log(message); },
+    async session(message) { /* session is active */ console.log(message); },
+    async error(message) { /* error in authentication flow */ console.log(message); }
+  }
+}
+
+export default (req, res) => NextAuth(req, res, options)
---
+++ src/pages/overview.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import { useSession, signIn, signOut } from 'next-auth/client'
+import DefaultLayout from "../layouts/default";
+
+const Overview: FC = () => {
+  const [session, loading] = useSession();
+  return (
+    <>
+      <DefaultLayout
+        title="Overview"
+        sidebar="overview"
+        description="You can see your portfolios estimated value & progress below"
+      >
+        {
+          session
+            ?
+            (<>
+              Signed in as {session.user.email} <br />
+              <button onClick={() => signOut()}>Sign out</button>
+            </>)
+            :
+            loading ?
+              (
+                <div className="space-y-2">
+                  <div className="h-4 bg-gray-400 rounded"></div>
+                  <div className="h-4 bg-gray-400 rounded w-5/6"></div>
+                </div>
+              )
+              :
+              (<>
+                Not signed in <br />
+                <button onClick={() => signIn()}>Sign in</button>
+              </>)
+        }
+      </DefaultLayout>
+    </>
+  )
+}
+
+export default Overview;
