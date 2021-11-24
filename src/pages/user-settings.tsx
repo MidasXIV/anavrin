05d0f4974170a18d520c6844110a916fca55928a
MidasXIV <amanshaikh.arc@gmail.com>
2021-11-24 23:15:42 +0400

    feat: Use a from factory to display forms and exchange buttons

--- src/pages/user-settings.tsx
+++ src/pages/user-settings.tsx
@@ -1,85 +1,39 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/client";
-import { InputWrapper, Input, Button } from "@mantine/core";
-import { useForm } from "@mantine/hooks";
 import DefaultLayout from "../layouts/default";
-import { encrypt, decrypt } from "../lib/crypto";
+import * as exchanges from "../components/exchanges-form";
+import ExchangeFormFactory from "../components/exchange-form-factory";
 
 const Overview: FC = () => {
   const [session, loading] = useSession();
-  const form = useForm({
-    initialValues: {
-      binanceAPI: "",
-      binanceSecret: ""
-    }
-
-    // validationRules: {
-    //   email: (value) => /^\S+@\S+$/.test(value),
-    // },
-  });
   const isSignedIn = loading ? "" : Boolean(session?.user) ?? false;
+  const [exchangeKey, exchangeKeyPanel] = useState<exchanges.ExchangeKeys>(null);
+
+  const onExchangeButtonClick = (exchange: exchanges.ExchangeKeys) => {
+    // Pass in the exchange name which can be understood by the Form Factory.
+    // sets the secondary Panel.
+    exchangeKeyPanel(exchange);
+  };
+
+  const Exchanges = exchanges.list.map(exchange =>
+    // eslint-disable-next-line import/namespace
+    exchanges[exchange]({ onClick: onExchangeButtonClick })
+  );
+
   return (
     <>
       <DefaultLayout title="User setting" sidebar="" description="Update user profile">
-        {/* 
-        1. Choose Exchange Account
-        2. Transactions only / Balance only
-        3. API Limitations
-        4. Scan API key / API Secret
-        5. Add connection
-        */}
         <div className="w-full h-full flex flex-row">
           <div className="dashboard-primary-panel">
-            <h1 className="text-2xl mb-2">Connect anavrin to an exchange account.</h1>
+            <h1 className="text-2xl mb-2">
+              Connect <span className="font-semibold">Anavrin</span> to an exchange account.
+            </h1>
             <p className="text-md text-gray-600">We currently only support Binance exchange.</p>
-            <form
-              onSubmit={form.onSubmit(values => {
-                console.log(values);
-                const encryptedAPIKey = encrypt(values.binanceAPI);
-                console.log(`Encrypted message: ${encryptedAPIKey}`);
-                const decryptedAPIKey = decrypt(encryptedAPIKey);
-                console.log(`Decrypted message: ${decryptedAPIKey}`);
-              })}
-            >
-              <section className="py-8">
-                <InputWrapper
-                  id="binance-api-key"
-                  required
-                  label="Binance API Key"
-                  description="Please enter your binance API key"
-                  className="pb-2"
-                >
-                  <Input
-                    id="binance-api-key"
-                    placeholder="API key"
-                    variant="filled"
-                    value={form.values.binanceAPI}
-                    onChange={event => form.setFieldValue("binanceAPI", event.currentTarget.value)}
-                  />
-                </InputWrapper>
-                <InputWrapper
-                  id="binance-api-secret"
-                  required
-                  label="Binance API Secret"
-                  description="Please enter your binance API Secret"
-                  className="pb-2"
-                >
-                  <Input
-                    id="binance-api-secret"
-                    placeholder="API secret"
-                    variant="filled"
-                    value={form.values.binanceSecret}
-                    onChange={event =>
-                      form.setFieldValue("binanceSecret", event.currentTarget.value)
-                    }
-                  />
-                </InputWrapper>
-
-                <Button type="submit">Connect</Button>
-              </section>
-            </form>
+            <section className="grid grid-cols-4 gap-4 my-4">{Exchanges}</section>
           </div>
-          <div className="dashboard-secondary-panel">Secondary Panel</div>
+          <div className="dashboard-secondary-panel">
+            <ExchangeFormFactory exchange={exchangeKey} />
+          </div>
         </div>
       </DefaultLayout>
     </>
