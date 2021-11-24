90f42708eceb3c9bda9d879981e2a2e6baf0e319
MidasXIV <amanshaikh.arc@gmail.com>
2021-11-24 23:27:17 +0400

    Mxiv/user details form (#51)
    # Summary
    With this PR Anavrin now shows a User's image if a user is logged in; A User settings page is added for the user to update details. A User can not enter exchange information to connect information to different exchanges; Mantine components are used for UI and some hooks logic; The data entered in the exchange form is encrypted.
    
    ## Features: 
    * Show user image in Header if user is logged in.
    * A User settings page is added to allows users to change their details. Currently only allows user to connect exchanges to anavrin.
    * Use a from factory to display forms and exchange buttons.
    
    ## Chores
    * use next/Link instead of a tag in header and sidebar component.
    * Update eslint rules. had some issues with enums.
    * Add mantine components  scaffolding in add and doc file.
    * Add dependencies
       * `mantine/core` -> for mantine components like dropdown menu.
       * `mantine/hooks` -> for forms.
       *  `react-jss` -> UI styling required by mantine.
       * `crypto` -> used to encrypt user entered data in forms.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,7 @@
+import { useSession } from "next-auth/client";
 import { FC } from "react";
+import { Menu, MenuItem, Divider, Text } from "@mantine/core";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 type HeaderProps = {
@@ -6,51 +9,61 @@ type HeaderProps = {
   description: string;
 };
 
-const Header: FC<HeaderProps> = ({ title, description }) => (
-  <div className={styles.header}>
-    <div className={styles.leftHeader}>
-      <div className={`${styles.leftContentPanel} ${styles.content}`}>
-        <h3 className="text-2xl font-black">{title}</h3>
-        <p className="text-xs font-thin font-sans">{description}</p>
-      </div>
-    </div>
-    <div className={styles.rightHeader}>
-      {/* <div className={`${styles.content} ${styles.rightBtnPanel}`}> */}
-      <div className={`${styles.content} ${styles.rightContentPanel}`}>
-        <button className="relative inline-block" type="button">
-          <img
-            className="inline-block w-10 h-10 object-cover rounded-lg"
-            src="//images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-            alt="Profile"
-          />
-          <span className="absolute bottom-0 right-0 inline-block w-3 h-3 -mr-1 bg-green-600 border-2 border-white rounded-full" />
-        </button>
-        {/* <div className="flex flex-row justify-around w-full h-full">
-            <button type="button" className={styles.headerBtn}>
-              Account
-            </button>
-            <button type="button" className={styles.headerBtn}>
-              Sign out
-            </button>
-            <button type="button" className={styles.headerBtn}>
-              Sign out
-            </button>
-          </div>
+const Header: FC<HeaderProps> = ({ title, description }) => {
+  const [session, loading] = useSession();
+  const defaultUserImage =
+    "//images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80";
+  const userImage = loading ? defaultUserImage : session?.user?.image ?? defaultUserImage;
 
-          <div className="flex flex-row justify-around w-full h-full">
-            <button type="button" className={styles.headerBtn}>
-              Account
-            </button>
-            <button type="button" className={styles.headerBtn}>
-              Sign out
-            </button>
-            <button type="button" className={styles.headerBtn}>
-              Sign out
-            </button>
-          </div> */}
+  return (
+    <div className={styles.header}>
+      <div className={styles.leftHeader}>
+        <div className={`${styles.leftContentPanel} ${styles.content}`}>
+          <h3 className="text-2xl font-black">{title}</h3>
+          <p className="text-xs font-thin font-sans">{description}</p>
+        </div>
+      </div>
+      <div className={styles.rightHeader}>
+        <div className={`${styles.content} ${styles.rightContentPanel}`}>
+          <Menu
+            // trigger="hover"
+            controlRefProp="ref"
+            control={
+              <button className="relative inline-block" type="button">
+                <img
+                  className="inline-block w-12 h-12 object-cover rounded-lg"
+                  src={userImage}
+                  alt="Profile"
+                />
+                <span className="absolute bottom-0 right-0 inline-block w-3 h-3 -mr-1 bg-green-600 border-2 border-white rounded-full" />
+              </button>
+            }
+            menuPosition={{ top: 53, right: 0 }}
+          >
+            <MenuItem>
+              <Link href="./user-settings">Settings</Link>
+            </MenuItem>
+            <MenuItem>
+              <a href="./user-settings">Messages</a>
+            </MenuItem>
+            <MenuItem>Gallery</MenuItem>
+            <MenuItem
+              rightSection={
+                <Text size="sm" color="gray">
+                  ⌘K
+                </Text>
+              }
+            >
+              Search
+            </MenuItem>
+            <Divider />
+            <MenuItem disabled>Delete my data</MenuItem>
+            <MenuItem color="red">Delete account</MenuItem>
+          </Menu>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,183 +2,203 @@ import type { AppProps } from "next/app";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/index.css";
-import React, { FC } from "react";
+import { useEffect, FC } from "react";
 import Head from "next/head";
 import { Provider } from "next-auth/client";
+import { JssProvider, createGenerateId } from "react-jss";
+import { MantineProvider, NormalizeCSS, GlobalStyles } from "@mantine/core";
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Provider session={pageProps.session}>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.getElementById("mantine-ssr-styles");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <>
+      <Provider session={pageProps.session}>
+        <JssProvider generateId={createGenerateId({ minify: true })}>
+          <Head>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+            />
 
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="shortcut icon" href="/favicon.ico" />
 
-        <meta name="application-name" content="Anavrin App" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Anavrin" />
-        <meta name="description" content="Crypto portfolio manager" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+            <meta name="application-name" content="Anavrin App" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-title" content="Anavrin" />
+            <meta name="description" content="Crypto portfolio manager" />
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+            <meta name="msapplication-TileColor" content="#2B5797" />
+            <meta name="msapplication-tap-highlight" content="no" />
+            <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
+            <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
 
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
 
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2048-2732.jpg"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2732-2048.jpg"
-          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1668-2388.jpg"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2388-1668.jpg"
-          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1536-2048.jpg"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2048-1536.jpg"
-          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1668-2224.jpg"
-          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2224-1668.jpg"
-          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1620-2160.jpg"
-          media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2160-1620.jpg"
-          media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1284-2778.jpg"
-          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2778-1284.jpg"
-          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1170-2532.jpg"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2532-1170.jpg"
-          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1125-2436.jpg"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2436-1125.jpg"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1242-2688.jpg"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2688-1242.jpg"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-828-1792.jpg"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1792-828.jpg"
-          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1242-2208.jpg"
-          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-2208-1242.jpg"
-          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-750-1334.jpg"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1334-750.jpg"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-640-1136.jpg"
-          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="icons/apple-splash-1136-640.jpg"
-          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2048-2732.jpg"
+              media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2732-2048.jpg"
+              media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1668-2388.jpg"
+              media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2388-1668.jpg"
+              media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1536-2048.jpg"
+              media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2048-1536.jpg"
+              media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1668-2224.jpg"
+              media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2224-1668.jpg"
+              media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1620-2160.jpg"
+              media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2160-1620.jpg"
+              media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1284-2778.jpg"
+              media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2778-1284.jpg"
+              media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1170-2532.jpg"
+              media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2532-1170.jpg"
+              media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1125-2436.jpg"
+              media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2436-1125.jpg"
+              media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1242-2688.jpg"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2688-1242.jpg"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-828-1792.jpg"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1792-828.jpg"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1242-2208.jpg"
+              media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-2208-1242.jpg"
+              media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-750-1334.jpg"
+              media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1334-750.jpg"
+              media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-640-1136.jpg"
+              media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            />
+            <link
+              rel="apple-touch-startup-image"
+              href="icons/apple-splash-1136-640.jpg"
+              media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            />
 
-        {/* <link rel='apple-touch-startup-image' href='/static/images/apple_splash_2048.png' sizes='2048x2732' />
+            {/* <link rel='apple-touch-startup-image' href='/static/images/apple_splash_2048.png' sizes='2048x2732' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1668.png' sizes='1668x2224' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1536.png' sizes='1536x2048' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1125.png' sizes='1125x2436' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_1242.png' sizes='1242x2208' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_750.png' sizes='750x1334' />
 <link rel='apple-touch-startup-image' href='/static/images/apple_splash_640.png' sizes='640x1136' /> */}
-      </Head>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </Provider>
-  </>
-);
-
+          </Head>
+          <MantineProvider
+            theme={{
+              /** Put your mantine theme override here */
+              colorScheme: "light"
+            }}
+          >
+            <main>
+              <NormalizeCSS />
+              <GlobalStyles />
+              <Component {...pageProps} />
+            </main>
+          </MantineProvider>
+        </JssProvider>
+      </Provider>
+    </>
+  );
+};
 export default MyApp;
