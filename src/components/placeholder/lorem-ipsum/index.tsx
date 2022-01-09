42e0095f68b383e3de9c76db2fc8968611011da3
MidasXIV <amanshaikh.arc@gmail.com>
2022-01-10 00:21:49 +0400

    feat: adds placeholder Lorem-Ipsum component

---
+++ src/components/placeholder/lorem-ipsum/index.tsx
@@ -0,0 +1,51 @@
+import { FC } from "react";
+
+const LoremIpsum: FC = () => (
+  <>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare, ante id ornare iaculis,
+      nibh massa luctus lacus, commodo bibendum libero mi ut leo. Quisque tincidunt ex quam, nec
+      semper nunc hendrerit et. Nulla vulputate imperdiet urna sit amet faucibus. Integer quis
+      libero leo. Vestibulum bibendum lacus nec lectus vestibulum dictum. In hac habitasse platea
+      dictumst. Sed egestas urna non pharetra efficitur. Praesent tincidunt nisl ut purus venenatis
+      faucibus. Donec dignissim id nisl non lacinia.
+    </p>
+    <p>
+      Curabitur dignissim rutrum egestas. Nunc vel urna non justo bibendum volutpat ultricies id
+      dolor. Pellentesque ultricies neque mi, a venenatis purus suscipit in. Donec iaculis est eu
+      facilisis porttitor. Quisque eu tortor hendrerit, accumsan enim nec, suscipit tellus. Donec
+      fermentum mollis nulla, sed ultricies purus hendrerit tristique. Vivamus ligula ligula,
+      eleifend non augue ac, aliquet scelerisque orci. Donec condimentum sollicitudin volutpat.
+      Aliquam odio urna, dictum vitae porttitor et, placerat at ipsum. Aliquam iaculis accumsan
+      justo eget lacinia.
+    </p>
+    <p>
+      Aenean quis mauris id nunc consectetur cursus in id purus. Proin ullamcorper tortor in metus
+      faucibus, vel semper magna bibendum. Morbi ultrices pharetra purus, luctus aliquet orci
+      sollicitudin nec. Sed porta ullamcorper mauris vel malesuada. Nulla facilisi. Mauris pulvinar
+      urna metus. Praesent pellentesque in lorem at rutrum. Nam id est at ligula elementum
+      condimentum. Cras rutrum id mi quis imperdiet. Pellentesque id convallis mauris, at
+      scelerisque nulla. Fusce vulputate justo vel vulputate accumsan. Duis venenatis nisl sem, id
+      convallis orci sollicitudin sit amet. Quisque vel augue id nisi efficitur sodales. Vestibulum
+      eleifend, diam quis mattis pretium, lacus mauris porttitor felis, non interdum sapien sapien
+      in nulla. Sed elementum, turpis at sagittis imperdiet, velit erat convallis nunc, non laoreet
+      erat massa scelerisque turpis. Ut auctor, justo ac vestibulum molestie, nunc lectus auctor
+      mauris, sed congue urna orci rutrum orci.
+    </p>
+    <p>
+      Aenean ut porta augue. Maecenas condimentum malesuada turpis rhoncus semper. Ut egestas nulla
+      a mi accumsan dictum. Praesent pulvinar dui at viverra rutrum. Aenean dictum, mi eu egestas
+      commodo, ante magna vulputate ante, quis euismod augue lacus eget sem. Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit. Cras dignissim, tortor vitae vulputate viverra, dolor metus
+      ullamcorper augue, a fringilla est sem vitae leo.
+    </p>
+    <p>
+      Suspendisse potenti.Vestibulum elementum est a dictum gravida.Sed a feugiat elit, dictum
+      convallis nunc.Nam pellentesque orci et augue cursus, non sollicitudin ligula semper.Nunc sed
+      eros eu magna sollicitudin consequat.Aliquam sed nulla eu magna porttitor hendrerit quis
+      pretium ligula.Proin tristique tortor eu nisl porttitor, eu eleifend leo imperdiet.
+    </p>
+  </>
+);
+
+export default LoremIpsum;
