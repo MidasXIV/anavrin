c5022ca48ea66b6eaf28d3c6d39751474eb615ae
MidasXIV <amanshaikh.arc@gmail.com>
2022-01-22 00:30:20 +0400

    feat: Show seconday panel as a Drawer

--- src/pages/user-settings.tsx
+++ src/pages/user-settings.tsx
@@ -1,11 +1,14 @@
 import { FC, useState } from "react";
+import { Drawer } from "@mantine/core";
 import { useSession, signIn, signOut } from "next-auth/client";
 import DefaultLayout from "../layouts/default";
 import * as exchanges from "../components/exchanges-form";
 import ExchangeFormFactory from "../components/exchange-form-factory";
+import { isMobileUI } from "../lib/viewport";
 
 const Overview: FC = () => {
   const [session, loading] = useSession();
+  const [opened, setOpened] = useState(false);
   const isSignedIn = loading ? "" : Boolean(session?.user) ?? false;
   const [exchangeKey, exchangeKeyPanel] = useState<exchanges.ExchangeKeys>(null);
 
@@ -13,6 +16,10 @@ const Overview: FC = () => {
     // Pass in the exchange name which can be understood by the Form Factory.
     // sets the secondary Panel.
     exchangeKeyPanel(exchange);
+    // Only in Mobile UI should the Secondary Panel as a drawer.
+    if (isMobileUI()) {
+      setOpened(true);
+    }
   };
 
   const Exchanges = exchanges.list.map(exchange =>
@@ -33,6 +40,15 @@ const Overview: FC = () => {
           </div>
           <div className="dashboard-secondary-panel">
             <ExchangeFormFactory exchange={exchangeKey} />
+            <Drawer
+              opened={opened}
+              onClose={() => setOpened(false)}
+              title="Register"
+              padding="xl"
+              size="xl"
+            >
+              <ExchangeFormFactory exchange={exchangeKey} />
+            </Drawer>
           </div>
         </div>
       </DefaultLayout>
