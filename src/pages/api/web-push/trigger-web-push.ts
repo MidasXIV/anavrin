ecaea3d824f51c51b82c7df7f4069815cdcfe331
Aman Shaikh <a.shaikh@esri.com>
2022-09-17 00:36:46 +0400

    Configure webpush options before using sendNotification

--- src/pages/api/web-push/trigger-web-push.ts
+++ src/pages/api/web-push/trigger-web-push.ts
@@ -2,13 +2,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import webPush from "web-push";
 import createHandlers from "../../../lib/rest-utils";
 
+const vapidDetails = {
+  publicKey: process.env.VAPID_PUBLIC_KEY,
+  privateKey: process.env.VAPID_PRIVATE_KEY,
+  subject: process.env.VAPID_SUBJECT
+};
+
+const options = {
+  TTL: 10000,
+  vapidDetails
+};
+
 const handlers = {
   POST: async (req: NextApiRequest, res: NextApiResponse) => {
     const { subscription } = req.body;
     webPush
       .sendNotification(
         subscription,
-        JSON.stringify({ title: "Hello Web Push", message: "Your web push notification is here!" })
+        JSON.stringify({ title: "Hello Web Push", message: "Your web push notification is here!" }),
+        options
       )
       .then(response => {
         res.writeHead(response.statusCode, response.headers).end(response.body);
