6747325d1b0fabcac890724d29616981394f0056
Aman Shaikh <a.shaikh@esri.com>
2023-01-16 23:18:45 +0400

    Add comments for isEmpty util function

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,3 +1,10 @@
+/**
+ * Function that checks if the input object is empty
+ *
+ * @param {Record<string, unknown>} obj - The input object to check
+ * @returns {boolean} - A boolean value indicating if the object is empty or not
+ */
 export default function isEmpty(obj: Record<string, unknown>): boolean {
+  // Get the keys of the object and check if the length of the keys is 0
   return Object.keys(obj).length === 0;
 }
