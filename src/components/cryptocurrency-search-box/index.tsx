a1f4dc312e7359fc06f5da700ba18a8239accfdb
Aman Shaikh <a.shaikh@esri.com>
2023-01-16 23:21:13 +0400

    create crypto-currency searchbox

---
+++ src/components/cryptocurrency-search-box/index.tsx
@@ -0,0 +1,76 @@
+import { useState, FC, useEffect, SetStateAction, Dispatch } from "react";
+import { Group, Avatar, Text, Autocomplete } from "@mantine/core";
+import { fetchCoinList } from "../../util/cryptocurrencyService";
+
+type CryptocurrencySearchBoxProps = {
+  setCyptocurrency: Dispatch<SetStateAction<string>>;
+};
+
+const CryptocurrencySearchBox: FC<CryptocurrencySearchBoxProps> = ({ setCyptocurrency }) => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  function AutoCompleteItem({ id, symbol, name, ...others }) {
+    return (
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      <div {...others} key={id}>
+        <Group>
+          <Avatar color="blue">
+            {name
+              .split(" ")
+              .map(part => part.charAt(0).toUpperCase())
+              .join("")}
+          </Avatar>
+
+          <div>
+            <Text>{name}</Text>
+            <Text size="xs" color="blue">
+              {symbol}
+            </Text>
+          </div>
+        </Group>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    setDisabled(true);
+
+    fetchCoinList().then(list => {
+      setDisabled(false);
+      console.log(list);
+      setData(
+        list.map(item => ({
+          value: item.id,
+          ...item
+        }))
+      );
+
+      // TODO: handle case for error
+    });
+  }, []);
+
+  return (
+    <Autocomplete
+      label="Choose cryptocurrency"
+      placeholder="Pick one"
+      disabled={disabled}
+      value={searchTerm}
+      onChange={setSearchTerm}
+      onItemSubmit={token => {
+        setCyptocurrency(token.id);
+      }}
+      itemComponent={AutoCompleteItem}
+      nothingFound="CoinGecko cannot find this token."
+      limit={6}
+      data={data}
+      filter={(_value, item) =>
+        item.name.toLowerCase().startsWith(_value.toLowerCase().trim()) ||
+        item.symbol.toLowerCase().startsWith(_value.toLowerCase().trim())
+      }
+    />
+  );
+};
+
+export default CryptocurrencySearchBox;
