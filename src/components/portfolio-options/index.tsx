3227d2904599ad1a8b82a8c977eddb5b88d91c56
Aman Shaikh <a.shaikh@esri.com>
2023-02-05 16:09:28 +0400

    Update portfolio options menu

--- src/components/portfolio-options/index.tsx
+++ src/components/portfolio-options/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import cn from "classnames";
-import { Tooltip } from "@mantine/core";
+import { Divider, Menu, MenuItem, Tooltip } from "@mantine/core";
+import { signOut, signIn } from "next-auth/react";
+import Link from "next/link";
 import styles from "./index.module.css";
 import TooltipWrapper from "../tooltip-wrapper";
 
@@ -100,7 +102,7 @@ const PortfolioOptions: FC<PortfolioOptionsProps> = ({
     </ul>
     {/* Options */}
     <ul className="nav flex flex-row xl:hidden">
-      <button type="button" className={styles.icon}>
+      {/* <button type="button" className={styles.icon}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-8"
@@ -115,7 +117,109 @@ const PortfolioOptions: FC<PortfolioOptionsProps> = ({
             d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
           />
         </svg>
-      </button>
+      </button> */}
+      <Menu
+        // trigger="hover"
+        controlRefProp="ref"
+        control={
+          // <button className="relative inline-block" type="button">
+          //   <Image
+          //     className="inline-block h-12 w-12 rounded-lg object-cover"
+          //     src={userImage}
+          //     alt="Profile"
+          //     layout="intrinsic"
+          //     width={48}
+          //     height={48}
+          //   />
+          //   <span className="absolute bottom-0 right-0 -mr-1 inline-block h-3 w-3 rounded-full border-2 border-white bg-green-600" />
+          // </button>
+          <button type="button" className={styles.icon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              />
+            </svg>
+          </button>
+        }
+        position="bottom"
+        placement="end"
+        gutter={6}
+      >
+        <MenuItem
+          color="orange"
+          icon={
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.81825 1.18188C7.64251 1.00615 7.35759 1.00615 7.18185 1.18188L4.18185 4.18188C4.00611 4.35762 4.00611 4.64254 4.18185 4.81828C4.35759 4.99401 4.64251 4.99401 4.81825 4.81828L7.05005 2.58648V9.49996C7.05005 9.74849 7.25152 9.94996 7.50005 9.94996C7.74858 9.94996 7.95005 9.74849 7.95005 9.49996V2.58648L10.1819 4.81828C10.3576 4.99401 10.6425 4.99401 10.8182 4.81828C10.994 4.64254 10.994 4.35762 10.8182 4.18188L7.81825 1.18188ZM2.5 9.99997C2.77614 9.99997 3 10.2238 3 10.5V12C3 12.5538 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2238 12.2239 9.99997 12.5 9.99997C12.7761 9.99997 13 10.2238 13 10.5V12C13 13.104 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2238 2.22386 9.99997 2.5 9.99997Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              />
+            </svg>
+          }
+        >
+          Save
+        </MenuItem>
+        <MenuItem
+          color="red"
+          icon={
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              />
+            </svg>
+          }
+        >
+          Delete
+        </MenuItem>
+        <Divider />
+        <MenuItem
+          color="green"
+          icon={
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4.81812 4.68161C4.99386 4.85734 4.99386 5.14227 4.81812 5.318L3.08632 7.0498H11.9135L10.1817 5.318C10.006 5.14227 10.006 4.85734 10.1817 4.68161C10.3575 4.50587 10.6424 4.50587 10.8181 4.68161L13.3181 7.18161C13.4939 7.35734 13.4939 7.64227 13.3181 7.818L10.8181 10.318C10.6424 10.4937 10.3575 10.4937 10.1817 10.318C10.006 10.1423 10.006 9.85734 10.1817 9.68161L11.9135 7.9498H3.08632L4.81812 9.68161C4.99386 9.85734 4.99386 10.1423 4.81812 10.318C4.64239 10.4937 4.35746 10.4937 4.18173 10.318L1.68173 7.818C1.50599 7.64227 1.50599 7.35734 1.68173 7.18161L4.18173 4.68161C4.35746 4.50587 4.64239 4.50587 4.81812 4.68161Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              />
+            </svg>
+          }
+          onClick={togglePortfolioAnalysisPanel}
+        >
+          Analyse
+        </MenuItem>
+      </Menu>
     </ul>
   </div>
 );
