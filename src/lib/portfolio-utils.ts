3345d237581618f0ac22a4891a686fab55da311d
Aman Shaikh <a.shaikh@esri.com>
2023-02-11 23:07:53 +0400

    Add getPortfolioTableSchema utility function * returns Table Schema based on portfolioType

--- src/lib/portfolio-utils.ts
+++ src/lib/portfolio-utils.ts
@@ -1,6 +1,26 @@
+import {
+  CryptoPortfolioSchema,
+  DFMDividendPortfolioSchema,
+  DividendPortfolioSchema
+} from "./table-schema";
+
 enum PortfolioType {
   STOCK = "stock",
-  CRYPTO = "crypto"
+  CRYPTO = "crypto",
+  DFM = "dfm"
 }
 
-export default PortfolioType;
+const getPortfolioTableSchema = (portfolioType: PortfolioType) => {
+  switch (portfolioType) {
+    case PortfolioType.CRYPTO:
+      return CryptoPortfolioSchema;
+    case PortfolioType.DFM:
+      return DFMDividendPortfolioSchema;
+    case PortfolioType.STOCK:
+      return DividendPortfolioSchema;
+    default:
+      return DividendPortfolioSchema;
+  }
+};
+
+export { PortfolioType, getPortfolioTableSchema };
