25bdf53768ebe7eb12891ce9fbc8bc0b6ccfc763
Aman Shaikh <a.shaikh@esri.com>
2023-02-19 01:03:04 +0400

    Add Add-crypto-form component * Component to take in Token, holdings, and fiat spent to acquire the holdings. * Uses crypto-table to populate crypto information.

---
+++ src/components/add-asset-modal/add-crypto-form.tsx
@@ -0,0 +1,101 @@
+import { useForm } from "@mantine/hooks";
+import { InputWrapper, Input } from "@mantine/core";
+import { FC, useState } from "react";
+import CryptocurrencySearchBox from "../cryptocurrency-search-box";
+import SlideToSubmit from "../slide-to-submit";
+import { fetchCoinInfo } from "../../util/cryptocurrencyService";
+import CryptoInformationTable from "./crypto-information-table";
+
+const AddCryptoForm: FC<unknown> = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [tokenInformation, setTokenInformation] = useState("");
+  const form = useForm({
+    initialValues: {
+      token: "",
+      holdings: 0,
+      fiat: 0
+    }
+
+    // validationRules: {
+    //   email: (value) => /^\S+@\S+$/.test(value),
+    // },
+  });
+
+  const fetchTokenInformation = token => {
+    fetchCoinInfo(token)
+      .then(tokenInfo => {
+        setTokenInformation(tokenInfo);
+      })
+      .catch(e => {
+        console.error(e);
+      })
+      .finally(() => {
+        console.log("request completed");
+      });
+  };
+
+  return (
+    <form>
+      <section className="py-2 px-2">
+        <InputWrapper
+          id="cryptocurrency-searchbox"
+          required
+          label="Token name"
+          description="Select Cryptocurrency"
+          className="pb-2"
+        >
+          <CryptocurrencySearchBox
+            hideHeader
+            setCyptocurrency={token => {
+              setSearchTerm(token);
+              fetchTokenInformation(token);
+              form.setFieldValue("token", token);
+            }}
+          />
+        </InputWrapper>
+        <InputWrapper
+          id="cryptocurrency-holdings"
+          required
+          label="Token Holdings"
+          description="Enter the number of tokens you own"
+          className="pb-2"
+        >
+          <Input
+            id="cryptocurrency-holdings"
+            placeholder="1000"
+            variant="filled"
+            value={form.values.holdings}
+            onChange={event =>
+              form.setFieldValue("holdings", parseFloat(event.currentTarget.value))
+            }
+          />
+        </InputWrapper>
+
+        <InputWrapper
+          id="cryptocurrency-fiat"
+          required
+          label="Fiat"
+          description="Fiat spent on acquiring holdings"
+          className="pb-2"
+        >
+          <Input
+            id="cryptocurrency-fiat"
+            placeholder="500"
+            variant="filled"
+            value={form.values.fiat}
+            onChange={event => form.setFieldValue("fiat", parseFloat(event.currentTarget.value))}
+          />
+        </InputWrapper>
+
+        {tokenInformation ? <CryptoInformationTable coin={tokenInformation} /> : null}
+        <SlideToSubmit
+          onSubmit={form.onSubmit(values => {
+            console.log({ ...values, tokenInformation });
+          })}
+        />
+      </section>
+    </form>
+  );
+};
+
+export default AddCryptoForm;
