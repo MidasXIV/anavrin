dc51e12d465bc88bb7e41bf0bb91a894943aee5d
Aman Shaikh <ama10117@esri.com>
2023-07-02 00:46:15 +0400

    lint/ pretty

--- src/components/website/theme-toggle/theme-toggle.tsx
+++ src/components/website/theme-toggle/theme-toggle.tsx
@@ -1,37 +1,34 @@
 import { useMantineColorScheme, SegmentedControl, Group, Center, Box } from "@mantine/core";
 import PlusIconSVG from "../../icons/AddItemIcon";
 
-const ThemeToggle = () => {
+const ThemeToggle = () => (
   // const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
-  return (
-    <Group position="center" my="xl">
-      <SegmentedControl
-        // value={colorScheme}
-        // onChange={(value: "light" | "dark") => toggleColorScheme(value)}
-        data={[
-          {
-            value: "light",
-            label: (
-              <Center>
-                <PlusIconSVG width={15} height={15}/>
-                <Box ml={10}>Light</Box>
-              </Center>
-            )
-          },
-          {
-            value: "dark",
-            label: (
-              <Center>
-                <PlusIconSVG width={15} height={15}/>
-                <Box ml={10}>Dark</Box>
-              </Center>
-            )
-          }
-        ]}
-      />
-    </Group>
-  );
-};
-
+  <Group position="center" my="xl">
+    <SegmentedControl
+      // value={colorScheme}
+      // onChange={(value: "light" | "dark") => toggleColorScheme(value)}
+      data={[
+        {
+          value: "light",
+          label: (
+            <Center>
+              <PlusIconSVG width={15} height={15} />
+              <Box ml={10}>Light</Box>
+            </Center>
+          )
+        },
+        {
+          value: "dark",
+          label: (
+            <Center>
+              <PlusIconSVG width={15} height={15} />
+              <Box ml={10}>Dark</Box>
+            </Center>
+          )
+        }
+      ]}
+    />
+  </Group>
+);
 export default ThemeToggle;
