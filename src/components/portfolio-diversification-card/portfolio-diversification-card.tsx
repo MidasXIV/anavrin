9caf1bc8d8689cb2ab1ca7e1de25c229e701e51a
Aman Shaikh <ama10117@esri.com>
2023-07-18 00:29:34 +0400

    Create portfolio-diversification-card componenet * shows how the fiat is diversified across all portfolios

---
+++ src/components/portfolio-diversification-card/portfolio-diversification-card.tsx
@@ -0,0 +1,23 @@
+import { useResizeObserver } from "@mantine/hooks";
+import { FC } from "react";
+import { getPortfolioDiversificationChartData } from "../../lib/portfolio-utils";
+import RingChart from "../ring-chart/ring-chart";
+
+interface IPortfolioDiversificationCardProps {
+  portfolios: Portfolio[];
+}
+
+const PortfolioDiversificationCard: FC<IPortfolioDiversificationCardProps> = ({ portfolios }) => {
+  const [ref, size] = useResizeObserver();
+  const ringChartData = getPortfolioDiversificationChartData(portfolios);
+  return (
+    <div
+      className="max-w-full rounded-md border border-gray-200 bg-charcoal-400 p-6 text-sm hover:bg-charcoal-900"
+      ref={ref}
+    >
+      <RingChart sections={ringChartData} width={size.width < 0 ? 0 : size.width} />
+    </div>
+  );
+};
+
+export default PortfolioDiversificationCard;
