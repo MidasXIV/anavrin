a9464e707a00c70e6cb3b26e35919e8ff912ca11
Aman Shaikh <ama10117@esri.com>
2023-07-18 00:30:15 +0400

    create portfolio-overview-card component to be shown on dashboard landing.

---
+++ src/components/portfolio-overview-card/portfolio-overview-card.tsx
@@ -0,0 +1,34 @@
+import { ScrollArea, Divider } from "@mantine/core";
+import { FC } from "react";
+
+interface IPortfolioOverviewCardProps {
+  portfolio: Portfolio;
+}
+
+const PortfolioOverviewCard: FC<IPortfolioOverviewCardProps> = ({ portfolio }) => {
+  const totalInvestment = portfolio.items.reduce((acc, item) => acc + item.fiat, 0);
+  return (
+    <div className="block h-full w-full rounded-md border border-gray-200 bg-charcoal-400 p-6 text-sm hover:bg-charcoal-900">
+      <h5 className="mb-2 font-mono text-xl font-bold tracking-tight text-white">
+        Portfolio {portfolio._id}
+      </h5>
+      <p className="my-2 text-gray-400">Asset Type: {portfolio.assetType}</p>
+      <ScrollArea h={200} offsetScrollbars scrollbarSize={4}>
+        {portfolio.items.map((item, index) => (
+          <div key={index} className="mb-4">
+            <p className="font-medium text-white">{item.token}</p>
+            <p className="text-gray-400">
+              Holdings: {item.holdings} {item.token}
+            </p>
+            <p className="text-gray-400">Fiat Value: {item.fiat} USD</p>
+          </div>
+        ))}
+      </ScrollArea>
+
+      <Divider my="sm" />
+      <p className="my-2 text-gray-400">Total investment: {totalInvestment}</p>
+    </div>
+  );
+};
+
+export default PortfolioOverviewCard;
