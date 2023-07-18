40b140af08f99ea5d736dba4c1e5de859c71de52
Aman Shaikh <ama10117@esri.com>
2023-07-18 23:37:48 +0400

    add typegaurds to portfolio item

--- src/components/portfolio-overview-card/portfolio-overview-card.tsx
+++ src/components/portfolio-overview-card/portfolio-overview-card.tsx
@@ -1,5 +1,6 @@
 import { ScrollArea, Divider } from "@mantine/core";
 import { FC } from "react";
+import { isCryptoPortfolioItem } from "../../lib/portfolio-asset-utils";
 
 interface IPortfolioOverviewCardProps {
   portfolio: Portfolio;
@@ -15,10 +16,13 @@ const PortfolioOverviewCard: FC<IPortfolioOverviewCardProps> = ({ portfolio }) =
       <p className="my-2 text-gray-400">Asset Type: {portfolio.assetType}</p>
       <ScrollArea h={200} offsetScrollbars scrollbarSize={4}>
         {portfolio.items.map((item, index) => (
-          <div key={index} className="mb-4">
-            <p className="font-medium text-white">{item.token}</p>
+          <div key={`portfolio-item-${index + 1}`} className="mb-4">
+            <p className="font-medium text-white">
+              {isCryptoPortfolioItem(item) ? item.token : item.ticker}
+            </p>
             <p className="text-gray-400">
-              Holdings: {item.holdings} {item.token}
+              Holdings: {isCryptoPortfolioItem(item) ? item.holdings : item.shares}{" "}
+              {isCryptoPortfolioItem(item) ? item.token : item.ticker}
             </p>
             <p className="text-gray-400">Fiat Value: {item.fiat} USD</p>
           </div>
