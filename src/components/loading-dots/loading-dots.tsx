c470dbf1229c66fc01a1f4a2635ee744010a07aa
Aman Shaikh <ama10117@esri.com>
2023-11-05 00:58:22 +0400

    Add Box-button component

---
+++ src/components/loading-dots/loading-dots.tsx
@@ -0,0 +1,11 @@
+import s from "./loading-dots.module.css";
+
+const LoadingDots = () => (
+  <span className={s.root}>
+    <span />
+    <span />
+    <span />
+  </span>
+);
+
+export default LoadingDots;
