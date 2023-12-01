cebbb40c7b7cd89b29c6ce3b356e4b3e13284922
Aman Shaikh <ama10117@esri.com>
2023-12-01 19:00:19 +0400

    use tremor for ring chart

--- src/components/ring-chart/ring-chart.tsx
+++ src/components/ring-chart/ring-chart.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable react/require-default-props */
 import { RingProgress, Text, Group, RingProgressProps } from "@mantine/core";
+import { DonutChart } from "@tremor/react";
 import { FC, useState } from "react";
 
-// type RingChartSections = Array<{ value: number; color: string; tooltip: string }>;
-type RingChartSections = RingProgressProps["sections"];
+type RingChartSections = Array<{ value: number; tooltip: string }>;
+// type RingChartSections = RingProgressProps["sections"];
 
+/**
+ * category -> tool tip text
+ * index -> numerical value
+ */
 interface RingChartProps {
   sections: RingChartSections;
   width?: number;
 }
+
+const valueFormatter = number => `$ ${new Intl.NumberFormat("us").format(number).toString()}`;
 
 const RingChart: FC<RingChartProps> = ({
   sections = [],
@@ -18,8 +25,16 @@ const RingChart: FC<RingChartProps> = ({
   width?: number;
 }) => (
   <Group position="center">
-    <RingProgress size={width} thickness={12} roundCaps sections={sections} />
+    {/* <RingProgress size={width} thickness={12} roundCaps sections={sections} /> */}
     {/* <Text>Hovered section: {hovered === -1 ? "none" : hovered}</Text> */}
+    <DonutChart
+      className=""
+      data={sections}
+      category="value"
+      index="value"
+      colors={["rose", "yellow", "orange", "indigo", "blue", "emerald"]}
+      // onValueChange={(v) => setValue(v)}
+    />
   </Group>
 );
 export default RingChart;
