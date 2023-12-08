61054eeae770b0ff58eeb4ccab34f138391f7595
Aman Shaikh <ama10117@esri.com>
2023-12-09 00:23:48 +0400

    improve sidebar layout, making sliding menu

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -114,24 +114,45 @@ const Sidebar: FC<SidebarProps> = ({ select }) => (
 
             <Menu.Dropdown>
               <Menu.Item
-                icon={
-                  <svg
-                    className="bi bi-collection icon"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14.5 13.5h-13A.5.5 0 011 13V6a.5.5 0 01.5-.5h13a.5.5 0 01.5.5v7a.5.5 0 01-.5.5zm-13 1A1.5 1.5 0 010 13V6a1.5 1.5 0 011.5-1.5h13A1.5 1.5 0 0116 6v7a1.5 1.5 0 01-1.5 1.5h-13zM2 3a.5.5 0 00.5.5h11a.5.5 0 000-1h-11A.5.5 0 002 3zm2-2a.5.5 0 00.5.5h7a.5.5 0 000-1h-7A.5.5 0 004 1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                }
+                // icon={
+                //   <svg
+                //     className="bi bi-collection icon"
+                //     width="1em"
+                //     height="1em"
+                //     viewBox="0 0 16 16"
+                //     fill="currentColor"
+                //     xmlns="http://www.w3.org/2000/svg"
+                //   >
+                //     <path
+                //       fillRule="evenodd"
+                //       d="M14.5 13.5h-13A.5.5 0 011 13V6a.5.5 0 01.5-.5h13a.5.5 0 01.5.5v7a.5.5 0 01-.5.5zm-13 1A1.5 1.5 0 010 13V6a1.5 1.5 0 011.5-1.5h13A1.5 1.5 0 0116 6v7a1.5 1.5 0 01-1.5 1.5h-13zM2 3a.5.5 0 00.5.5h11a.5.5 0 000-1h-11A.5.5 0 002 3zm2-2a.5.5 0 00.5.5h7a.5.5 0 000-1h-7A.5.5 0 004 1z"
+                //       clipRule="evenodd"
+                //     />
+                //   </svg>
+                // }
+                component="a"
+                href="/simulator/dubai-financial-markets"
               >
-                <Link href="/simulator/dubai-financial-markets">DFM</Link>
+                <div className={clsx(styles.icon, { [styles.active]: select === "simulator" })}>
+                  <button className="relative inline-block" type="button">
+                    <svg
+                      className="bi bi-collection icon"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14.5 13.5h-13A.5.5 0 011 13V6a.5.5 0 01.5-.5h13a.5.5 0 01.5.5v7a.5.5 0 01-.5.5zm-13 1A1.5 1.5 0 010 13V6a1.5 1.5 0 011.5-1.5h13A1.5 1.5 0 0116 6v7a1.5 1.5 0 01-1.5 1.5h-13zM2 3a.5.5 0 00.5.5h11a.5.5 0 000-1h-11A.5.5 0 002 3zm2-2a.5.5 0 00.5.5h7a.5.5 0 000-1h-7A.5.5 0 004 1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  {/* <Link href="/simulator/dubai-financial-markets">DFM</Link> */}
+                  DFM
+                </div>
               </Menu.Item>
               <Divider />
               <Menu.Item
@@ -160,7 +181,7 @@ const Sidebar: FC<SidebarProps> = ({ select }) => (
       </ul>
 
       <ul className="nav flex flex-row sm:flex-col">
-        <li className={cn(styles.icon, { [styles.active]: select === "news" })}>
+        <li className={clsx(styles.icon, { [styles.active]: select === "news" })}>
           <Link href="/register">
             <svg
               className="bi bi-file-earmark-text icon"
