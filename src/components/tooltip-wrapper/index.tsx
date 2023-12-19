13c9ed982232fd88d9048bf515cb6b254434cc3a
Aman Shaikh <ama10117@esri.com>
2023-12-20 00:40:43 +0400

    Update tooltip to use shadcn tooltip

--- src/components/tooltip-wrapper/index.tsx
+++ src/components/tooltip-wrapper/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
-import { Tooltip } from "@mantine/core";
+// import { Tooltip } from "@mantine/core";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 type TooltipWrapperProps = {
   label: string;
@@ -9,14 +11,16 @@ type TooltipWrapperProps = {
 };
 
 const TooltipWrapper: FC<TooltipWrapperProps> = ({ label, color, children }) => (
-  <Tooltip
-    label={label}
-    color={color}
-    transitionProps={{ transition: "fade", duration: 300, timingFunction: "ease" }}
-    withArrow
-  >
-    {children}
-  </Tooltip>
+  <TooltipProvider>
+    <Tooltip
+    // color={color}
+    // transitionProps={{ transition: "fade", duration: 300, timingFunction: "ease" }}
+    // withArrow
+    >
+      <TooltipTrigger>{children}</TooltipTrigger>
+      <TooltipContent>{label}</TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
 );
 
 export default TooltipWrapper;
