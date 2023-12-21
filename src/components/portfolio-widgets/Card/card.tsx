d6fceda2a6bb75958c6a957b8f5bb894320ca832
Aman Shaikh <ama10117@esri.com>
2023-12-22 01:45:12 +0400

    Add padding to card button

--- src/components/portfolio-widgets/Card/card.tsx
+++ src/components/portfolio-widgets/Card/card.tsx
@@ -6,13 +6,14 @@ const Card = ({
   showFooter = false,
   children,
   customFooter = false,
-  footer = null
+  footer = null,
+  className = null
 }) => (
-  <div className="flex h-full w-full flex-col rounded-lg bg-gray-100">
+  <div className={`flex h-full w-full flex-col rounded-lg bg-gray-100 shadow-lg ${className}`}>
     {showHeader ? (
       <div className="border-success dark:border-success-300 inline-flex justify-between border-b-2 px-3 pb-1 pt-2 text-xs text-neutral-600 dark:text-neutral-50">
         <span>{header}</span>
-        <button type="button" className="hover:bg-gray-200">
+        <button type="button" className="rounded-lg px-2 py-1 hover:bg-gray-200">
           <InfoIcon />
         </button>
       </div>
