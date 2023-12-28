0145a851d5c75318da7951740304c9cffecee568
Aman Shaikh <ama10117@esri.com>
2023-12-28 16:47:55 +0400

    add compact variant styling to economic-events-panel

--- src/components/economic-events-panel/index.tsx
+++ src/components/economic-events-panel/index.tsx
@@ -7,6 +7,13 @@ import {
 } from "../../utils/timeAndDateHelpers";
 import api from "../../services/create-service";
 
+const VARIANT = {
+  compact: "compact",
+  default: "default"
+} as const;
+
+type VARIANT = keyof typeof VARIANT;
+
 const LoadingListItem = () => (
   <li>
     <a href="#" className="block items-center p-3 hover:bg-gray-100 sm:flex dark:hover:bg-gray-700">
@@ -43,7 +50,15 @@ const LoadingEconomicEventDayBlock = () => (
   </div>
 );
 
-const ListItem = ({ event, day }: { event: IEcnomicEvent; day: IEcnomicCalandarItemDay }) => {
+const ListItem = ({
+  event,
+  day,
+  variant
+}: {
+  event: IEcnomicEvent;
+  day: IEcnomicCalandarItemDay;
+  variant: VARIANT;
+}) => {
   const { actual, expected, for: _for, impact, prior, release, time } = event;
   const eventDate = buildEventDate(day, time);
   const eventDateString = formatDateString(eventDate);
@@ -53,7 +68,10 @@ const ListItem = ({ event, day }: { event: IEcnomicEvent; day: IEcnomicCalandarI
     <li>
       <a
         href="#"
-        className="block items-center p-3 hover:bg-gray-100 sm:flex dark:hover:bg-gray-700"
+        className={clsx("block items-center hover:bg-gray-100 sm:flex dark:hover:bg-gray-700", {
+          "px-2 py-2": variant === VARIANT.compact,
+          "p-3": variant === VARIANT.default
+        })}
       >
         <div className="w-full">
           <div className="text-left text-xs font-semibold">
@@ -61,7 +79,14 @@ const ListItem = ({ event, day }: { event: IEcnomicEvent; day: IEcnomicCalandarI
             {eventDateString} <span>( {timeDifferenceString} )</span>
           </div>
           <div className="flex flex-row justify-between py-2 text-base font-normal text-gray-600 dark:text-gray-400">
-            <span className="text-2xl font-medium text-gray-900 dark:text-white">{release} </span>
+            <span
+              className={clsx("font-medium text-gray-900 dark:text-white", {
+                "text-lg": variant === VARIANT.compact,
+                "text-2xl": variant === VARIANT.default
+              })}
+            >
+              {release}{" "}
+            </span>
             <div
               className={clsx("h-6 w-6 rounded-md border-2 border-black", {
                 "bg-red-500": impact === "3",
@@ -95,35 +120,218 @@ const ListItem = ({ event, day }: { event: IEcnomicEvent; day: IEcnomicCalandarI
   );
 };
 
-const EconomicEventDayBlock = ({ event }: { event: IEcnomicCalandarItem }) => {
+const EconomicEventDayBlock = ({
+  event,
+  variant,
+  id
+}: {
+  id: string;
+  event: IEcnomicCalandarItem;
+  variant: VARIANT;
+}) => {
   const { day, events } = event;
   return (
-    <div className="mb-4 rounded-lg border border-gray-100 bg-gray-50 p-5 dark:border-gray-700 dark:bg-gray-800">
-      <time className="text-lg font-semibold text-gray-900 dark:text-white">{day}</time>
+    <div
+      id={id}
+      className={clsx(
+        "mb-4 rounded-lg border border-gray-100 bg-gray-50 dark:border-gray-700 dark:bg-gray-800",
+        {
+          "p-2": variant === VARIANT.compact,
+          "p-5": variant === VARIANT.default
+        }
+      )}
+    >
+      <time
+        className={clsx("font-semibold text-gray-900 dark:text-white", {
+          "text-md": variant === VARIANT.compact,
+          "text-lg": variant === VARIANT.default
+        })}
+      >
+        {day}
+      </time>
       <ol className="divider-gray-200 mt-3 divide-y dark:divide-gray-700">
         {events.map((economicEvent, index) => (
-          <ListItem key={`economic-event-${index + 1}`} event={economicEvent} day={day} />
+          <ListItem
+            key={`economic-event-${index + 1}`}
+            event={economicEvent}
+            day={day}
+            variant={variant}
+          />
         ))}
       </ol>
     </div>
   );
 };
 
-const EconomicEventsPanel: FC<any> = () => {
+interface IEconomicEventsPanelProps {
+  variant?: VARIANT; // Define the variant prop
+}
+
+const mockEE = [
+  {
+    day: "Mon Dec 25",
+    events: [
+      {
+        time: "",
+        release: "No economic releases"
+      }
+    ]
+  },
+  {
+    day: "Tue Dec 26",
+    events: [
+      {
+        time: "9:00 AM",
+        release: "FHFA Housing Price Index",
+        impact: "1",
+        for: "Oct",
+        actual: "0.3%",
+        expected: "NA",
+        prior: "0.7%"
+      },
+      {
+        time: "9:00 AM",
+        release: "S&P Case-Shiller Home Price Index",
+        impact: "1",
+        for: "Oct",
+        actual: "4.9%",
+        expected: "5.0%",
+        prior: "3.9%"
+      }
+    ]
+  },
+  {
+    day: "Wed Dec 27",
+    events: [
+      {
+        time: "",
+        release: "No economic releases"
+      }
+    ]
+  },
+  {
+    day: "Thu Dec 28",
+    events: [
+      {
+        time: "8:30 AM",
+        release: "Initial Claims",
+        impact: "2",
+        for: "12/23",
+        actual: "-",
+        expected: "207K",
+        prior: "205K"
+      },
+      {
+        time: "8:30 AM",
+        release: "Continuing Claims",
+        impact: "2",
+        for: "12/16",
+        actual: "-",
+        expected: "NA",
+        prior: "1865K"
+      },
+      {
+        time: "8:30 AM",
+        release: "Adv. Intl. Trade in Goods",
+        impact: "1",
+        for: "Nov",
+        actual: "-",
+        expected: "NA",
+        prior: "-$89.8B"
+      },
+      {
+        time: "8:30 AM",
+        release: "Adv. Retail Inventories",
+        impact: "1",
+        for: "Nov",
+        actual: "-",
+        expected: "NA",
+        prior: "0.0%"
+      },
+      {
+        time: "8:30 AM",
+        release: "Adv. Wholesale Inventories",
+        impact: "1",
+        for: "Nov",
+        actual: "-",
+        expected: "NA",
+        prior: "-0.2%"
+      },
+      {
+        time: "10:00 AM",
+        release: "Pending Home Sales",
+        impact: "2",
+        for: "Nov",
+        actual: "-",
+        expected: "0.5%",
+        prior: "-1.5%"
+      },
+      {
+        time: "10:30 AM",
+        release: "EIA Natural Gas Inventories",
+        impact: "1",
+        for: "12/23",
+        actual: "-",
+        expected: "NA",
+        prior: "-87 bcf"
+      },
+      {
+        time: "11:00 AM",
+        release: "EIA Crude Oil Inventories",
+        impact: "3",
+        for: "12/23",
+        actual: "-",
+        expected: "NA",
+        prior: "+2.909M"
+      }
+    ]
+  },
+  {
+    day: "Fri Dec 29",
+    events: [
+      {
+        time: "9:45 AM",
+        release: "Chicago PMI",
+        impact: "2",
+        for: "Dec",
+        actual: "-",
+        expected: "50.0",
+        prior: "55.8"
+      }
+    ]
+  }
+];
+
+const EconomicEventsPanel: FC<IEconomicEventsPanelProps> = ({ variant = VARIANT.default }) => {
   const [loading, setLoading] = useState(false);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    setLoading(true);
+    // setLoading(true);
 
-    api.fetchEconomicEvents().then(({ status, data }) => {
-      setLoading(false);
+    // api.fetchEconomicEvents().then(({ status, data }) => {
+    //   setLoading(false);
 
-      if (status === 200) {
-        setEvents(data.events);
-      }
-      // TODO: handle case for error
-    });
+    //   if (status === 200) {
+    //     setEvents(data.events);
+    //     console.log(data.events)
+    //   }
+    //   // TODO: handle case for error
+    // });
+
+    setEvents(mockEE);
+
+    // Mon = 1
+    // const currentDayInNumberFormat = new Date().getDay();
+
+    // const targetElement = document.getElementById(
+    //   `economic-event-block-${currentDayInNumberFormat}`
+    // );
+    // console.log(targetElement);
+    // if (targetElement) {
+    //   const { offsetTop } = targetElement;
+    //   window.scrollTo({ top: offsetTop, behavior: "smooth" });
+    // }
   }, []);
 
   return (
@@ -131,7 +339,12 @@ const EconomicEventsPanel: FC<any> = () => {
       {loading ? <LoadingEconomicEventDayBlock /> : null}
       {events
         ? events.map((event, index) => (
-            <EconomicEventDayBlock key={`economic-event-block-${index + 1}`} event={event} />
+            <EconomicEventDayBlock
+              id={`economic-event-block-${index + 1}`}
+              key={`economic-event-block-${index + 1}`}
+              event={event}
+              variant={variant}
+            />
           ))
         : null}
     </div>
