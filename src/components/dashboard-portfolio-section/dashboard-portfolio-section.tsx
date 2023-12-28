bd388cecc18f201af222489f118d517c36bf7360
Aman Shaikh <ama10117@esri.com>
2023-12-28 16:48:24 +0400

    revamp dash-board-portfolio selection by making function toggle groups to filter portfolios

--- src/components/dashboard-portfolio-section/dashboard-portfolio-section.tsx
+++ src/components/dashboard-portfolio-section/dashboard-portfolio-section.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { AssetType } from "lib/portfolio-utils";
 import PortfolioOverviewCard from "../portfolio-overview-card/portfolio-overview-card";
 
 interface IDashboardPortfolioSectionProps {
@@ -9,19 +11,46 @@ interface IDashboardPortfolioSectionProps {
 const DashboardPortfolioSection: FC<IDashboardPortfolioSectionProps> = ({
   portfolios,
   onPortfolioSelect
-}) => (
-  <section className="flex h-full w-full items-center justify-center">
-    <div className="flex w-full grid-cols-4 flex-col gap-3 lg:grid">
-      <div className="col-span-3 max-w-full">
-        <section className="grid h-full auto-cols-[250px] grid-flow-col gap-3">
-          {portfolios.map(portfolio => (
-            <PortfolioOverviewCard
-              key={portfolio._id}
-              portfolio={portfolio}
-              onPortfolioSelect={onPortfolioSelect}
-              variant="compact"
-            />
+}) => {
+  const [filterAsset, setFilterAsset] = useState("ALL");
+  return (
+    <section className="outline-test flex h-full w-full flex-col items-center justify-center overflow-hidden p-2">
+      <div className="outline-test flex w-full items-center justify-between py-3">
+        <span className="text-xl font-bold">Portfolios</span>
+        <ToggleGroup
+          type="single"
+          onValueChange={selectGroupItem => {
+            setFilterAsset(selectGroupItem);
+          }}
+        >
+          <ToggleGroupItem key="ALL" value="ALL" className="text-xs">
+            ALL
+          </ToggleGroupItem>
+          {Object.keys(AssetType).map(assetTypeKey => (
+            <ToggleGroupItem key={assetTypeKey} value={AssetType[assetTypeKey]} className="text-xs">
+              {AssetType[assetTypeKey].toUpperCase()}
+            </ToggleGroupItem>
           ))}
+        </ToggleGroup>
+      </div>
+
+      <div className="outline-test w-full">
+        <section className="grid h-full auto-cols-[250px] grid-flow-col gap-3 overflow-auto p-2 pb-6">
+          {portfolios
+            .filter(portfolio => {
+              if (filterAsset === "ALL") {
+                return true;
+              }
+              return portfolio.assetType === filterAsset;
+            })
+            .map(portfolio => (
+              <PortfolioOverviewCard
+                key={portfolio._id}
+                portfolio={portfolio}
+                onPortfolioSelect={onPortfolioSelect}
+                variant="compact"
+              />
+            ))}
           {portfolios.map(portfolio => (
             <PortfolioOverviewCard
               key={portfolio._id}
@@ -48,8 +77,8 @@ const DashboardPortfolioSection: FC<IDashboardPortfolioSectionProps> = ({
           ))}
         </section>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default DashboardPortfolioSection;
