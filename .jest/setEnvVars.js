724f0471ff500f26130c9ce8f5401d5bbf3b9bef
Aman Shaikh <ama10117@esri.com>
2024-04-16 16:33:57 +0400

    update jest config

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,3 +1,7 @@
 process.env.MONGODB_URI=
 process.env.MONGODB_DB=
 process.env.TEST_EMAIL=
+process.env.STRIPE_PUBLIC_KEY=
+process.env.STRIPE_SECRET_KEY=
+process.env.NGROK_DOMAIN=
+process.env.MERGENT_APIKEY=
