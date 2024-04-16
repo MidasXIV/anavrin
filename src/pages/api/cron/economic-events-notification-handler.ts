dd61f990f18c4e0dbb7b22f5fbf9b15e0a0076f9
Aman Shaikh <ama10117@esri.com>
2024-04-16 16:32:48 +0400

    update readme

--- src/pages/api/cron/economic-events-notification-handler.ts
+++ src/pages/api/cron/economic-events-notification-handler.ts
@@ -51,36 +51,10 @@ const handlers = {
       const db = await connectToDatabase();
       const userModel = new UserModel(db);
       const subscriptions = await userModel.getAllUserSubscription();
-      console.log(subscriptions);
 
       const baseUrl = process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : "http://localhost:3000";
-
-      // const bodyData = {
-      //   title,
-      //   message,
-      //   subscription: {
-      //     _id: "660c5e0b516486cf72faeb70",
-      //     endpoint:
-      //       "https://fcm.googleapis.com/fcm/send/eYpXLlcP0Zc:APA91bEzx7juWOqAsr3sk40IgVsEpypogGIUndXHVdkBryE8kJcNMMfqaYq9n5L2paKpN604uvOxc3vMQseZly6TJXxjNOdyP7TvqLChuQBrGFUzdeP3nNYksO2PmuQ2s5VF2QWFBVdD",
-      //     expirationTime: null,
-      //     keys: {
-      //       p256dh:
-      //         "BOANl-WB_eBJXPgWxDv2KezKZOssXVRKjjb2iq2yuiie74cBDclw-ifblF0QvKu58On8Gq7xcu4nVZy-xMlObHs",
-      //       auth: "Hr3oDqOlVd6KSZCrzxJhlA"
-      //     }
-      //   }
-      // };
-
-      // const response = await fetch(`${baseUrl}/api/web-push/trigger-web-push`, {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json"
-      //     // 'Content-Type': 'application/x-www-form-urlencoded',
-      //   },
-      //   body: JSON.stringify(bodyData)
-      // });
 
       // Array to hold fetch promises
       const fetchPromises = subscriptions.map(async subscription => {
@@ -113,33 +87,7 @@ const handlers = {
       // Execute all fetch promises concurrently and wait for all to settle
       await Promise.allSettled(fetchPromises);
 
-      // const data: EcnomicEventsDTO = await response.json();
-
-      // const { events } = data;
-      // console.log(data);
       res.status(200).json({ message: "Notification sent" });
-
-      // const finvizEconomicCalendarModel = new FinvizEconomicCalendarModel();
-      // const result: EcnomicEventsResponse = await finvizEconomicCalendarModel.getEcnomicEvents();
-
-      // if (Result.isFail(result)) {
-      //   const resultError = Result.getError(result);
-      //   switch (resultError.type) {
-      //     case "UnableToParseData":
-      //       res.status(500).json(resultError);
-      //       break;
-      //     case "UnableToFetchData":
-      //       res.status(500).json(resultError);
-      //       break;
-      //     default:
-      //       res.status(500).json(resultError);
-      //       break;
-      //   }
-      //   console.log(resultError);
-      // }
-
-      // const resultSuccess = Result.getValue(result);
-      // res.status(200).json(resultSuccess);
     } catch (err) {
       // Report the error to metrics + logging app
       console.log(err);
