38d49ad2b36af129eb0ab8c8c4b347353202088c
Aman Shaikh <ama10117@esri.com>
2024-08-07 22:53:42 +0400

    create calendar widget

---
+++ src/components/dashboard-portfolio-section/calendar-chart.tsx
@@ -0,0 +1,182 @@
+import clsx from "clsx";
+import { FC } from "react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { valueFormatter } from "@/utils/timeAndDateHelpers";
+import { Separator } from "../ui/separator";
+
+interface DashboardCalendarChartProps {
+  data: CrptoPortfolioValue;
+  mode: CalendarMode;
+}
+
+function percentageDifference(valueA: number, valueB: number): number {
+  if (valueB === 0) {
+    return 0;
+  } else {
+    const percentageDiff = ((valueA - valueB) / valueB) * 100;
+    return percentageDiff;
+  }
+}
+
+enum CalendarMode {
+  OneMonth = "1-month",
+  SixMonths = "6-months",
+  EntireYear = "entire-year",
+  YearToMonth = "year-to-month"
+}
+
+const DashboardCalendarChart: FC<DashboardCalendarChartProps> = ({ data, mode }) => {
+  const { quotes } = data;
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const daysOfWeek = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  const calculateMonthRanges = (mode: CalendarMode) => {
+    const months: { year: number; month: number }[] = [];
+    if (mode === CalendarMode.OneMonth) {
+      months.push({ year: currentYear, month: currentMonth });
+    } else if (mode === CalendarMode.SixMonths) {
+      for (let i = 0; i < 6; i++) {
+        const month = currentMonth - i;
+        const year = month < 0 ? currentYear - 1 : currentYear;
+        months.push({ year, month: (month + 12) % 12 });
+      }
+    } else if (mode === CalendarMode.EntireYear) {
+      for (let month = 0; month < 12; month++) {
+        months.push({ year: currentYear, month });
+      }
+    } else if (mode === CalendarMode.YearToMonth) {
+      for (let month = 0; month <= currentMonth; month++) {
+        months.push({ year: currentYear, month });
+      }
+    }
+    // return months.reverse();
+    return months;
+  };
+
+  const months = calculateMonthRanges(mode);
+  const startDate = new Date(currentYear, 0, 1);
+  const getQuoteIndex = (date: Date) => {
+    const diffTime = Math.abs(date.getTime() - startDate.getTime());
+    return Math.floor(diffTime / (1000 * 60 * 60 * 24)); // Difference in days
+  };
+
+  return (
+    <TooltipProvider>
+      <div
+        className={clsx({
+          "flex w-full flex-wrap items-center justify-center": true,
+          "md:grid md:grid-cols-2 lg:grid-cols-3": mode === CalendarMode.EntireYear || mode === CalendarMode.YearToMonth
+        })}
+      >
+        {months.map(({ year, month }) => {
+          const firstDayOfMonth = new Date(year, month, 1);
+          const daysInMonth = new Date(year, month + 1, 0).getDate();
+          const firstDayOfWeek = firstDayOfMonth.getDay() + (1 % 6);
+
+          return (
+            <div key={`${year}-${month}`} className="mb-8 w-full overflow-hidden rounded-lg">
+              <h2 className="mb-4 text-center font-chakra font-semibold">
+                {monthNames[month]} {year}
+              </h2>
+              <div className="grid grid-cols-7 gap-0 p-4" id="calendar">
+                {daysOfWeek.map(day => (
+                  <div
+                    key={day}
+                    className="w-full cursor-pointer rounded-xl px-4 py-3 text-center font-chakra font-semibold"
+                  >
+                    <span>{day}</span>
+                  </div>
+                ))}
+                {Array.from({ length: firstDayOfWeek }).map((_, index) => (
+                  <div
+                    key={index}
+                    className="w-full cursor-pointer rounded-xl px-4 py-3 text-center font-chakra"
+                  ></div>
+                ))}
+                {Array.from({ length: daysInMonth }).map((_, dayIndex) => {
+                  const day = dayIndex + 1;
+                  const date = new Date(year, month, day);
+                  const quoteIndex = getQuoteIndex(date);
+                  const quoteOfDay = quotes[quoteIndex];
+                  const quoteOfPreviousDay = quotes[quoteIndex - 1 || 0];
+                  const percentageDiff = percentageDifference(quoteOfDay?.value, quoteOfPreviousDay?.value);
+                  return (
+                    <Tooltip key={dayIndex}>
+                      <TooltipTrigger>
+                        <div className="w-full cursor-pointer rounded-xl border border-gray-300 text-center font-chakra">
+                          <div
+                            className={clsx({
+                              "h-full w-full rounded-lg border-[3px] px-3 py-2 text-center": true,
+                              "border-gray-900": !(day === currentDate.getDate() && month === currentDate.getMonth()),
+                              "border-teal-300": day === currentDate.getDate() && month === currentDate.getMonth(),
+                              "bg-gray-200": typeof quoteOfDay === "undefined",
+                              "bg-green-500": percentageDiff >= 15,
+                              "bg-green-400": percentageDiff >= 10,
+                              "bg-green-300": percentageDiff >= 5,
+                              "bg-green-200": percentageDiff >= 0,
+                              "bg-red-200": percentageDiff < 0,
+                              "bg-red-300": percentageDiff < -5,
+                              "bg-red-400": percentageDiff < -10,
+                              "bg-red-500": percentageDiff < -15
+                            })}
+                          >
+                            <span>{day}</span>
+                          </div>
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        {quoteOfDay ? (
+                          <>
+                            {quoteOfDay?.meta.map((metaItem, metaIndex) => (
+                              <p
+                                key={metaIndex}
+                                className="flex w-48 justify-between font-chakra font-semibold text-gray-400"
+                              >
+                                <span>{metaItem.item?.toUpperCase()}</span>
+                                <span>{valueFormatter(metaItem.value)}</span>
+                              </p>
+                            ))}
+                            <Separator className="my-1 bg-gray-600" />
+                            <p className="flex w-48 justify-between font-chakra font-semibold text-gray-400">
+                              <span>{"Total".toUpperCase()}</span>
+                              <span>{`${valueFormatter(quoteOfDay?.value)} (${percentageDifference(
+                                quoteOfDay?.value,
+                                quotes[0]?.value
+                              ).toPrecision(2)}%)`}</span>
+                            </p>
+                          </>
+                        ) : (
+                          <p>No data yet</p>
+                        )}
+                      </TooltipContent>
+                    </Tooltip>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </TooltipProvider>
+  );
+};
+
+export default DashboardCalendarChart;
---
+++ src/pages/dashboardv2.tsx
@@ -0,0 +1,140 @@
+import { clsx } from "clsx";
+import { FC, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useSearchParams } from "next/navigation";
+
+import SecondaryPanel from "@/components/secondary-panel";
+import Card from "@/components/portfolio-widgets/Card/card";
+import PortfolioDiversificationCard from "@/components/portfolio-diversification-card/portfolio-diversification-card";
+import PortfolioDashboardPanel from "@/components/portfolio-widgets/portfolio-dashboard-panel";
+import LoremIpsum from "@/components/placeholder/lorem-ipsum";
+import { useSession } from "next-auth/react";
+import DashboardGreeting from "@/components/portfolio-widgets/dashboard-greeting/dashboard-greeting";
+import AssetsComparisonAreaChart from "@/components/portfolio-widgets/assets-comparison-area-chart/assets-comparison-area-chart";
+import { isMobileUI } from "lib/viewport";
+import { Button } from "@/components/ui/button";
+import TrackerChart from "@/components/charting/tracker/tracker";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import TooltipWrapper from "@/components/tooltip-wrapper";
+import { valueFormatter } from "@/utils/timeAndDateHelpers";
+import TrackerBlock from "@/components/portfolio-widgets/tracker-block/tracker-block";
+import GoalsTrackerCard from "@/components/portfolio-widgets/goals-tracker-card/goal-tracker-card";
+import CostMarketValueChart from "@/components/portfolio-widgets/cost-market-value-chart/cost-market-value-chart";
+import DefaultLayout from "../layouts/default";
+import api from "../services/create-service";
+import DashboardPortfolioSection from "../components/dashboard-portfolio-section/dashboard-portfolio-section";
+import DashboardPortfolioSectionLoading from "../components/dashboard-portfolio-section/dashboard-portfolio-section-loading";
+import EconomicEventsPanel from "../components/economic-events-panel";
+import { createUrl } from "../utils/helper";
+import mockFetchUserPortfolioData from "../tests/mocks/mock-crypto-portfolio-2";
+import DashboardCalendarChart from "@/components/dashboard-portfolio-section/calendar-chart";
+import {
+  calculateCrptoPortfolioValue,
+  hydrateCryptoPortfolioItemsV2
+} from "lib/portfolio-asset-utils";
+import { StockLineChart } from "@/components/charting/stock-line-chart/stock-line-chart";
+
+const Dashboard: FC = () => {
+  const { data: session, status } = useSession();
+  const [hide, setHide] = useState(false);
+  const [opened, setOpened] = useState(false);
+  const [portfolioPerformanceStockLineChartData, setPortfolioPerformanceStockLineChartData] =
+    useState([]);
+  const [portfolioPerformance, setPortfolioPerformance] =
+    useState<CrptoPortfolioValue>(undefined);
+  const [portfolios, setPortfolios] = useState<Array<Portfolio>>([]);
+  const [isPortfolioFetched, setIsPortfolioFetched] = useState(false);
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const setSelectedPortfolio = selectedPortfolio => {
+    const newParams = new URLSearchParams(searchParams.toString());
+
+    if (selectedPortfolio) {
+      newParams.set("q", selectedPortfolio);
+    } else {
+      newParams.delete("q");
+    }
+
+    router.push(createUrl("portfolio", newParams));
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // const fetchUserPortfoliosResponse = await api.fetchUserPortfolio();
+        // const { portfolios: userPortfolios = [] } = fetchUserPortfoliosResponse.data;
+
+        const userPortfolios = mockFetchUserPortfolioData;
+
+        for (const userPortfolio of userPortfolios) {
+          const data = await hydrateCryptoPortfolioItemsV2(userPortfolio);
+          const portfolioPerformance = calculateCrptoPortfolioValue(data);
+          const portfolioPerformanceStockLineChartData = portfolioPerformance?.quotes.map((a,index) => {
+            return {
+              value: a.value,
+              time: a.date,
+              ticker: `Portfolio-${index+1}`
+            };
+          });
+          setPortfolioPerformance(portfolioPerformance);
+          setPortfolioPerformanceStockLineChartData(portfolioPerformanceStockLineChartData);
+          console.log(portfolioPerformance);
+        }
+
+        setPortfolios(userPortfolios);
+      } catch (error) {
+        console.error(error);
+        // Handle the error appropriately, such as displaying a message to the user
+      } finally {
+        setIsPortfolioFetched(true);
+        console.log("done");
+      }
+
+      return () => setPortfolios([]);
+    })();
+  }, []);
+
+  let Content = null;
+  if (!isPortfolioFetched) {
+    Content = <DashboardPortfolioSectionLoading />;
+  } else if (portfolios?.length > 0) {
+    Content = (
+      <section className="h-full w-full">
+        <DashboardCalendarChart data={portfolioPerformance} mode='year-to-month' />
+        <StockLineChart data={[portfolioPerformanceStockLineChartData]} />
+      </section>
+    );
+  } else {
+    Content = (
+      <div className="flex h-full flex-col items-center justify-center">
+        <p className="text-gray-500">You don&apos;t have any portfolios.</p>
+        <p className="text-gray-500">Create a portfolio to get started!</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <DefaultLayout
+        title="Dashboard"
+        sidebar="dashboard"
+        description="You can see your portfolios estimated value & progress below"
+      >
+        <div className="flex h-full w-full flex-1 flex-row overflow-auto rounded-xl bg-gray-300 sm:mb-1 sm:rounded-lg">
+          <div
+            className={clsx("dashboard-primary-panel overflow-y-auto", {
+              "sm:w-full": hide,
+              // "sm:w-2/5": !hide
+              "sm:w-full": !hide
+            })}
+          >
+            {Content}
+          </div>
+        </div>
+      </DefaultLayout>
+    </>
+  );
+};
+export default Dashboard;
